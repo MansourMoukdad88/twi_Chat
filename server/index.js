9b9c0cc7bccee3d1f22137fb73311ed0b0e0ae73
MansourMoukdad88 <m_sy@outlook.com>
2019-05-20 23:36:32 +0300

    [Add] Error Handler function

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const errorHandler = require('./handlers/error');
 
 const PORT = 8081;
 app.use(cors());
@@ -12,9 +13,11 @@ app.use(bodyParser.json());
 
 app.use(function(req,res,next){
     let err = new Error("Not Found");
+    //var a = console.log("Hiiiiiii")
     err.status = 404;
     next(err);
 });
+app.use(errorHandler);
 
 app.listen(PORT, function(){
     console.log(`Server is Listening on ${PORT}`)
